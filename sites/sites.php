39ce26b4f8ee862b845be3dbdaaa011d3c4afef1
stratosys <ashish@logbullit.com>
2016-10-10 19:08:57 +0000

    Commit while deploying site into Test environment

--- sites/sites.php
+++ sites/sites.php
@@ -54,4 +54,4 @@
  * @see http://drupal.org/documentation/install/multi-site
  */
 
-$sites['dev-testevolver-57ee9e98.evolverinc.tech'] = 'all';
+$sites['test-testevolver-57ee9e98.evolverinc.tech'] = 'all';
